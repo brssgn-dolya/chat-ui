84bf6d65d06f2dc635bc52e0af102136930199b1
Boris <brssgn@icloud.com>
2024-09-03 12:36:01 +0300

    Add Codable support

--- Sources/ExyteChat/Model/User.swift
+++ Sources/ExyteChat/Model/User.swift
@@ -19,4 +19,35 @@ public struct User: Codable, Identifiable, Hashable {
         self.avatarCachedImage = avatarCachedImage
         self.isCurrentUser = isCurrentUser
     }
+
+    private enum CodingKeys: String, CodingKey {
+        case id
+        case name
+        case avatarURL
+        case avatarCachedImageData
+        case isCurrentUser
+    }
+
+    public func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(id, forKey: .id)
+        try container.encode(name, forKey: .name)
+        try container.encode(avatarURL, forKey: .avatarURL)
+        try container.encode(isCurrentUser, forKey: .isCurrentUser)
+        let imageData = avatarCachedImage?.pngData()
+        try container.encode(imageData, forKey: .avatarCachedImageData)
+    }
+
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        id = try container.decode(String.self, forKey: .id)
+        name = try container.decode(String.self, forKey: .name)
+        avatarURL = try container.decode(URL?.self, forKey: .avatarURL)
+        isCurrentUser = try container.decode(Bool.self, forKey: .isCurrentUser)
+        if let imageData = try container.decodeIfPresent(Data.self, forKey: .avatarCachedImageData) {
+            avatarCachedImage = UIImage(data: imageData)
+        } else {
+            avatarCachedImage = nil
+        }
+    }
 }
