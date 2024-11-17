9ab176656bd41722fc97f39eab8c54f60d32a52e
Boris <brssgn@icloud.com>
2024-11-17 22:42:06 +0200

    Fix longpress gesture

--- Sources/ExyteChat/ChatView/UIList.swift
+++ Sources/ExyteChat/ChatView/UIList.swift
@@ -565,14 +565,8 @@ struct UIList<MessageContent: View, InputView: View>: UIViewRepresentable {
                     .transition(.scale)
                     .background(MessageMenuPreferenceViewSetter(id: row.id))
                     .rotationEffect(Angle(degrees: (type == .conversation ? 180 : 0)))
-//                    .applyIf(row.message.type != .call && row.message.type != .status) {
-//                        $0.onReplyGesture(replySymbolColor: chatTheme.colors.myMessage.opacity(0.7)) { [weak self] in
-//                            self?.viewModel.messageMenuActionInternal(message: row.message, action: DefaultMessageMenuAction.reply)
-//                        }
-//                    }
                     .applyIf(showMessageMenuOnLongPress) {
                         $0.gesture(LongPressGesture(minimumDuration: 0.15)
-                            .sequenced(before: DragGesture())
                             .onEnded({ _ in
                                 self.viewModel.messageMenuRow = row
                         }))
