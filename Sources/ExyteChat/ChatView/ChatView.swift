961f442841bc8f26e63e80e6a2772a28a49e0d0d
ybohdan <31662567+ybohdan@users.noreply.github.com>
2025-01-14 12:08:39 +0200

    Add PublicMessageStatusView

--- Sources/ExyteChat/ChatView/ChatView.swift
+++ Sources/ExyteChat/ChatView/ChatView.swift
@@ -675,3 +675,27 @@ public extension ChatView {
         return view
     }
 }
+
+public struct PublicMessageStatusView: View {
+    private let status: Message.Status
+    private let onRetry: () -> Void
+    @State var statusSize: CGSize = .zero
+    var isLastMessageForCurrentUser: Bool
+    
+    public init(status: Message.Status, isLastMessageForCurrentUser: Bool, onRetry: @escaping () -> Void) {
+        self.status = status
+        self.isLastMessageForCurrentUser = isLastMessageForCurrentUser
+        self.onRetry = onRetry
+    }
+
+    public var body: some View {
+        if !isLastMessageForCurrentUser {
+            MessageStatusView(status: status) {
+                if case let .error(draft) = status {
+                    #warning("handle retry")
+                }
+            }
+            .sizeGetter($statusSize)
+        }
+    }
+}
